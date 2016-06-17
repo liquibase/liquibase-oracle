6c1bec9d46fedb0e02c27734013951bbac74b6b5
Mark Jeffrey <m_jeffrey@multipharma.be>
2016-06-17 13:06:23 +0200

    Add the ability to add comments to columns, tables and views independently of the object creation.

---
+++ src/main/java/liquibase/ext/ora/comment/CommentOnChange.java
@@ -0,0 +1,71 @@
+package liquibase.ext.ora.comment;
+
+import liquibase.change.AbstractChange;
+import liquibase.change.Change;
+import liquibase.change.ChangeMetaData;
+import liquibase.change.DatabaseChange;
+import liquibase.database.Database;
+import liquibase.statement.SqlStatement;
+
+
+@DatabaseChange(name="commentOn", description = "Create or replace a comment on a table or a column", priority = ChangeMetaData.PRIORITY_DEFAULT)
+public class CommentOnChange extends AbstractChange {
+
+    private String schemaName;
+    private String tableName;
+    private String columnName;
+    private String comment;
+
+    public CommentOnChange() {
+    }
+
+    public SqlStatement[] generateStatements(Database database) {
+
+        String schemaName = getSchemaName() == null ? database.getDefaultSchemaName() : getSchemaName();
+        CommentOnStatement statement = new CommentOnStatement(schemaName, getTableName(), getColumnName(), getComment() );
+        return new SqlStatement[]{statement};
+    }
+
+    public String getConfirmationMessage() {
+        return columnName == null
+                ? "Comment has been added to " + getTableName()
+                : "Comment has been added to " + getTableName() + "." + getColumnName();
+    }
+
+    // Since "COMMENT ON" replaces the existing comment is is not good to remove it as we don;t know what the last one was.
+    protected Change[] createInverses() {
+        return new Change[]{};
+    }
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public void setSchemaName(String schemaName) {
+        this.schemaName = schemaName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
---
+++ src/test/java/liquibase/ext/ora/comment/CommentOnChangeTest.java
@@ -0,0 +1,122 @@
+package liquibase.ext.ora.comment;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import liquibase.change.Change;
+import liquibase.change.ChangeFactory;
+import liquibase.change.ChangeMetaData;
+import liquibase.changelog.ChangeLogParameters;
+import liquibase.changelog.ChangeSet;
+import liquibase.changelog.DatabaseChangeLog;
+import liquibase.database.Database;
+import liquibase.database.core.OracleDatabase;
+import liquibase.ext.ora.testing.BaseTestCase;
+import liquibase.parser.ChangeLogParserFactory;
+import liquibase.resource.FileSystemResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+import liquibase.sql.Sql;
+import liquibase.sqlgenerator.SqlGeneratorFactory;
+import liquibase.statement.SqlStatement;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommentOnChangeTest extends BaseTestCase {
+    @Before
+    public void setUp() throws Exception {
+        changeLogFile = "liquibase/ext/ora/comment/changelog.test.xml";
+        connectToDB();
+        cleanDB();
+    }
+
+    @Test
+    public void test() throws Exception {
+        if (connection == null) {
+            return;
+        }
+        liquiBase.update("null");
+
+    }
+
+    @Test
+    public void generateStatement() {
+        CommentOnChange change = new CommentOnChange();
+
+        change.setSchemaName("SCHEMA NAME");
+        change.setTableName("TABLE NAME");
+        change.setColumnName("COLUMN NAME");
+        change.setComment("My comment with 'quotes'");
+
+        SqlStatement[] statements = change.generateStatements(new OracleDatabase());
+        assertEquals(1, statements.length);
+        CommentOnStatement statement = (CommentOnStatement) statements[0];
+
+        assertEquals("SCHEMA NAME", statement.getSchemaName());
+        assertEquals("TABLE NAME", statement.getTableName());
+        assertEquals("COLUMN NAME", statement.getColumnName());
+        assertEquals("My comment with 'quotes'", statement.getComment());
+    }
+
+    @Test
+    public void getConfirmationMessage() {
+        CommentOnChange change = new CommentOnChange();
+
+        change.setTableName("TABLE_NAME");
+        change.setComment("COMMENT");
+
+        assertEquals("Comment has been added to " + change.getTableName(), change.getConfirmationMessage());
+    }
+
+    @Test
+    public void getChangeMetaData() {
+        CommentOnChange enableCheckChange = new CommentOnChange();
+
+        assertEquals("commentOn", ChangeFactory.getInstance().getChangeMetaData(enableCheckChange).getName());
+        assertEquals("Create or replace a comment on a table or a column", ChangeFactory.getInstance().getChangeMetaData(enableCheckChange).getDescription());
+        assertEquals(ChangeMetaData.PRIORITY_DEFAULT, ChangeFactory.getInstance().getChangeMetaData(enableCheckChange).getPriority());
+    }
+
+    @Test
+    public void parseAndGenerate() throws Exception {
+        if (connection == null) {
+            return;
+        }
+
+        Database database = liquiBase.getDatabase();
+        ResourceAccessor resourceAccessor = new FileSystemResourceAccessor("src/test/java");
+
+        ChangeLogParameters changeLogParameters = new ChangeLogParameters();
+
+
+        DatabaseChangeLog changeLog = ChangeLogParserFactory.getInstance().getParser(changeLogFile, resourceAccessor).parse(changeLogFile,
+                changeLogParameters, resourceAccessor);
+
+        changeLog.validate(database);
+        List<ChangeSet> changeSets = changeLog.getChangeSets();
+
+        assertExpectedQuery( changeFrom( changeSets, "column-comment"), "COMMENT ON COLUMN LBUSER.for_comment.id IS 'My comment ''quoted'''" );
+        assertExpectedQuery( changeFrom( changeSets, "table-comment"), "COMMENT ON TABLE LBUSER.for_comment IS 'My TABLE comment ''quoted'''" );
+        assertExpectedQuery( changeFrom( changeSets, "view-comment"), "COMMENT ON TABLE LBUSER.for_comment_view IS 'My VIEW comment'" );
+    }
+
+    private void assertExpectedQuery(Change change, String expectedSql) {
+        Database database = liquiBase.getDatabase();
+        Sql[] sqlStatements = SqlGeneratorFactory.getInstance().generateSql(change.generateStatements(database)[0], database);
+        assertEquals(1, sqlStatements.length);
+        assertEquals(expectedSql, sqlStatements[0].toSql());
+    }
+
+    private Change changeFrom(List<ChangeSet> changeSets, String changeSetId) {
+        for (ChangeSet changeSet : changeSets) {
+            if ( changeSetId.equals(changeSet.getId())){
+                List<Change> changes = changeSet.getChanges();
+                assertEquals(1, changes.size());
+                return changes.get(0);
+            }
+        }
+        throw new IllegalStateException("No change set with id " + changeSetId + " found");
+    }
+
+}
